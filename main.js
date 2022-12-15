9208924fa03029227eef03a869d6ddd6ad31bdbc
memoryfile <clewis100@pm.me>
2022-12-15 16:15:27 -0500

    Add JavaScript

---
+++ main.js
@@ -0,0 +1,31 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.sayTitle = function () {
+    console.log(title);
+  };
+}
+
+let myLibrary = [malibu, seaOfTranquility];
+
+function addBookToLibrary() {
+  if (Book.isBook(true)) {
+    myLibrary.push(this);
+  }
+}
+
+const malibu = new Book(
+  "Malibu Rising",
+  "Taylor Jenkins Reid",
+  "369 pages",
+  "Not read"
+);
+
+const seaOfTranquility = new Book(
+  "Sea of Tranquility",
+  "Emily St. John Mandel",
+  "255 pages",
+  "Not read"
+);
