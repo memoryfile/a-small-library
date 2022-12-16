b39dcc30862a8395fcb6fca326f1fda5d7b6d2e3
memoryfile <clewis100@pm.me>
2022-12-16 15:44:19 -0500

    Add initial custom book adding functionality

--- main.js
+++ main.js
@@ -22,11 +22,24 @@ const seaOfTranquility = new Book(
   "Not read"
 );
 
-let myLibrary = [malibu];
+let myLibrary = [malibu, seaOfTranquility];
+
+function clearForm() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").value = "";
+}
 
 function addBookToLibrary() {
-    myLibrary.push(seaOfTranquility);
-    console.log(myLibrary);
-  }
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+  // Pushes these variables individually instead of creating new Book with them
+  myLibrary.push(title, author, pages, read);
+  console.log(myLibrary);
+  clearForm();
+}
 
-addBookToLibrary();
+// addBookToLibrary();
