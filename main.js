6a93a60fe5025f70c8fa65d2df1a601c8af460e2
memoryfile <clewis100@pm.me>
2022-12-16 12:12:59 -0500

    Add JS to HTML and move + format JS code

--- main.js
+++ main.js
@@ -3,17 +3,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.sayTitle = function () {
-    console.log(title);
-  };
-}
-
-let myLibrary = [malibu, seaOfTranquility];
-
-function addBookToLibrary() {
-  if (Book.isBook(true)) {
-    myLibrary.push(this);
-  }
+  // this.sayTitle = function () {
+  //   console.log(title);
+  // };
 }
 
 const malibu = new Book(
@@ -29,3 +21,12 @@ const seaOfTranquility = new Book(
   "255 pages",
   "Not read"
 );
+
+let myLibrary = [malibu, seaOfTranquility];
+
+function addBookToLibrary() {
+  if (Book.isBook(true)) {
+    myLibrary.push(this);
+    console.log(myLibrary);
+  }
+}
