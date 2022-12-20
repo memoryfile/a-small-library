51761851919ce06e9743610e6eb57db9bdd93647
memoryfile <clewis100@pm.me>
2022-12-20 16:04:09 -0500

    Comment out updateCards and add documentation

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ const bookInfo = myLibrary.map(
   (Book) => `${Book.title} ${Book.author} ${Book.pages} ${Book.read}`
 );
 console.log(bookInfo);
+// Use bookInfo.length to auto-generate # of cards.
+console.log(bookInfo.length)
 
 const cardBookInfo = myLibrary.map(
   (Book) => `Title: ${Book.title} \n Author: ${Book.author} \n Total pages in book: ${Book.pages} \n Finished?: ${Book.read} \n \n`
@@ -52,6 +54,7 @@ function clearForm() {
   document.getElementById("read").value = "";
 }
 
+// Adds a book to the array and clears the form inputs. WIP: check for empty form.
 function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
@@ -61,30 +64,32 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title, author, pages, read));
     console.log(myLibrary);
     clearForm();
-    updateCards();
+    // updateCards();
     existingBooks();
   } else {
     alert("No");
   }
 }
 
+// Provides a legible list of all array items and adds it to a card.
 function existingBooks() {
   let wholeList = document.createElement("li");
   wholeList.innerText = cardBookInfo;
   wholeArrayList.appendChild(wholeList);
 }
 
+// // Provides the last array item and adds it to a card.
 function addBookToLastCard() {
   let wholeList = document.createElement("li");
   wholeList.innerText = itemBookInfo;
   lastCardList.appendChild(wholeList); 
 }
 
-// WIP
-function updateCards() {
-  list = "";
-  existingBooks();
-}
+// WIP - clears list of books and adds updated array list back.
+// function updateCards() {
+//   wholeArrayList = "";
+//   existingBooks();
+// }
 
 // myLibrary.forEach((item) => {
 //   let li = document.createElement("li");
