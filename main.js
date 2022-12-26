3240964ba6122be8b90252f6d344d3e2339696d1
memoryfile <clewis100@pm.me>
2022-12-26 17:44:32 -0500

    Add back working code to main.js

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
-// Define elements: layout (to append cards), etc.
-
-// Form elements
+let cards = document.getElementById("cardText");
 
 let title = document.getElementById("title").value;
 let author = document.getElementById("author").value;
 let pages = document.getElementById("pages").value;
 let read = document.getElementById("read").value;
 
-// Define array and elements in array
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const malibu = new Book(
+  "Malibu Rising",
+  "Taylor Jenkins Reid",
+  "369 pages",
+  "Not read"
+);
+
+const seaOfTranquility = new Book(
+  "Sea of Tranquility",
+  "Emily St. John Mandel",
+  "255 pages",
+  "Not read"
+);
+
+let myLibrary = [malibu, seaOfTranquility];
+
+let bookTitle = myLibrary[0].title;
+
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+  if (!(title == "" || author == "" || pages == "")) {
+    myLibrary.push(new Book(title, author, pages, read));
+    console.log(myLibrary);
+    cards.append(bookTitle);
+    clearForm();
+  } else {
+    alert("Please fill out every form entry.");
+  }
+}
 
 // Reimplement working functions and simplify them
 
