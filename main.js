22ee5240bb54392b8296bc919195cb13df24c21a
memoryfile <clewis100@pm.me>
2022-12-26 17:25:26 -0500

    Implement ability to clear form

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // Define elements: layout (to append cards), etc.
 
+// Form elements
+
+let title = document.getElementById("title").value;
+let author = document.getElementById("author").value;
+let pages = document.getElementById("pages").value;
+let read = document.getElementById("read").value;
+
 // Define array and elements in array
 
 // Reimplement working functions and simplify them
+
+function clearForm() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").value = "";
+}
